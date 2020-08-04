1013673f15dc6ccf1538f2ca0a5d0997bfeab998
dryabtse <tain.furrious@gmail.com>
2020-08-04 10:04:45 +1000

    Added more pre-flight checks, fixed formatting

--- fixShardNames.js
+++ fixShardNames.js
@@ -1,16 +1,23 @@
-var fixShardNames = function(dbName="config", dryRun=true, verbose=true) {
+var fixShardNames = function(dbName="config", dryRun=true, verbose=false) {
+
     function printIfVerbose(arg) {
         if (verbose == true)
             printjson(arg);
     };
+
     function detectDryRun() {
       if (dryRun == true) print("The script is running in dry run mode. No modifications will be done to the metadata");
     };
+
     function runPreFlightChecks() {
       print("Running pre-flight checks");
       checkIfDbExists(dbName);
+      checkIfMongos();
+      checkIfLiveCSRS();
+      checkServerVersion();
       print("Pre-flight checks completed successfully");
     };
+
     function checkIfDbExists(dbName) {
       const res = db.adminCommand({"listDatabases": 1});
       assert(res, "The listDatabases command failed");
@@ -28,27 +35,63 @@ var fixShardNames = function(dbName="config", dryRun=true, verbose=true) {
       });
       assert.eq(dbFound, true, "The target database could not be located");
     };
+
+    // To make sure we are not running the script on a live CSRS
+    function checkIfLiveCSRS() {
+      const res = db.serverCmdLineOpts();
+      assert(res, "The serverCmdLineOpts command failed");
+      assert(res.hasOwnProperty("ok"), "The ok field is not present");
+      assert.eq(1, res.ok, "Failed to obtain the startup configuration parameters");
+      assert(res.hasOwnProperty("parsed"), "The parsed field is not present");
+      assert(!res.parsed.hasOwnProperty("sharding"), "This server is running with sharding enabled. The CSRS should be running in standalone or non-configsvr replica set mode for the script to work");
+    };
+
+    // To make sure we are not running the script on a mongos
+    function checkIfMongos() {
+      const res = db.serverStatus();
+      assert(res, "The serverStatus command failed");
+      assert(res.hasOwnProperty("ok"), "The ok field is not present");
+      assert.eq(1, res.ok, "Failed to run serverStatus command");
+      assert(res.hasOwnProperty("process"), "The process field is not present");
+      assert.neq("mongos", res.process, "Mongos detected but this script is not meant to be run on a mongos");
+    };
+
+    function checkServerVersion() {
+      const supportedVersions = [ "36", "40", "42" ];
+      const res = db.serverStatus();
+      assert(res, "The serverStatus command failed");
+      assert(res.hasOwnProperty("ok"), "The ok field is not present");
+      assert.eq(1, res.ok, "Failed to run serverStatus command");
+      assert(res.hasOwnProperty("version"), "The version field is not present");
+      const verArray = res.version.split(".");
+      const majVer = verArray[0] + verArray[1];
+      assert.eq(true, supportedVersions.includes(majVer), "Unsupported version detected. Please reach out to Technical Support for assistance");
+    };
+
     function countShardIdDocs(shardId) {
       const chunkCount = db.chunks.count({"shard": shardId});
       const dbCount = db.databases.count({"primary": shardId});
       const shardsCount = db.shards.count({"_id": shardId});
       return chunkCount + dbCount + shardsCount;
     };
+
     function removeShard(oldId) {
       // we can't update "_id" and there is a unique index on "host" thus we need to remove the shard document first
-      var ret = db.shards.remove({"_id": oldId});
+      const ret = db.shards.remove({"_id": oldId});
       assert(ret, "Shard document could not be removed: " + oldId);
       assert(ret.hasOwnProperty("nRemoved"), "The nRemoved field is not present");
       assert.eq(1, ret.nRemoved, "Failed to remove shard document: \n shardId: " + oldId + " ret: " + ret);
       printIfVerbose(ret);
     };
+
     function insertShard(newDoc) {
-      ret = db.shards.insert(newDoc);
+      const ret = db.shards.insert(newDoc);
       assert(ret, "Shard document could not be inserted: \n newDoc: " + newDoc + " ret: " + ret);
       assert(ret.hasOwnProperty("nInserted"), "The nInserted field is not present");
       assert.eq(1, ret.nInserted, "Failed to insert shard document: \n newDoc: " + newDoc + " ret: " + ret);
-      printIfVerb= ose(ret);
+      printIfVerbose(ret);
     };
+
     function fixShards(oldId, newDoc) {
       print("Updating config.shards...");
       assert.neq(oldId, newDoc._id, "New shard Id should be different from the old shard Id");
@@ -56,6 +99,7 @@ var fixShardNames = function(dbName="config", dryRun=true, verbose=true) {
       insertShard(newDoc);
       print("Done updating config.shards");
     };
+    
     function fixDatabases(oldId, newId) {
       print("Updating config.databases...");
       const ret = db.databases.update({"primary": oldId}, {"$set": {"primary": newId}}, {multi: true, writeConcern: { w: "majority", wtimeout: 5000 }});
@@ -70,6 +114,7 @@ var fixShardNames = function(dbName="config", dryRun=true, verbose=true) {
       printIfVerbose(ret);
       print("Done updating config.databases");
     };
+    
     function fixChunks(oldId, newId) {
       print("Updating config.chunks...");
       // this could potentially update a great number of documents
@@ -84,14 +129,17 @@ var fixShardNames = function(dbName="config", dryRun=true, verbose=true) {
       printIfVerbose(ret);
       print("Done updating config.chunks");
     };
+    
     function fix(shardDoc) {
         printIfVerbose(shardDoc);
         var ret = { _id: shardDoc._id, needsFixing: false, fixed: false };
         const oldShardId = shardDoc._id;
+
         // set the new shard Id to match the replica set name
         shardDoc._id = shardDoc.host.split("/")[0];
         const newShardId = shardDoc._id;
         assert.gt(newShardId.length, 0, "New shard Id is too short");
+
         // We only need to modify the metadata if the shard Id does not match repl set Id
         if (newShardId != oldShardId) {
             ret.needsFixing = true;
@@ -107,15 +155,20 @@ var fixShardNames = function(dbName="config", dryRun=true, verbose=true) {
               ret.fixed = true;
             };
         };
+
         return ret;
     };
+
     try {
       runPreFlightChecks();
       detectDryRun();
+      
       // This is the only global that we should need
       db = db.getSiblingDB(dbName);
+
       const shards = db.shards.find().toArray();
       assert.gt(shards.length, 0, "No shard documents found");
+
       var executionResults = [];
       var res = null;
       shards.forEach(function(s) {
